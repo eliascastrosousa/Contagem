dc50a1f92139f9ec5610e87157d44d3fd62b03ee
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-05-18 11:09:41 -0300

    commit site

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function confirmar() {
+    var inicio = document.getElementById('inicio')
+    var i = Number(inicio.value)
+    var fim = document.getElementById('fim')
+    var f = Number(fim.value)
+    var msg = document.getElementById('msg')
     
+while (i > f){
+    msg.innerHTML = ('Comendo uma fatia de pizza')
 }
+
+
+//for (let pos in valores) {  // para cada posição dentro de valores, mostre o valores[pos]
+ //   console.log(valores[pos])
+//}
